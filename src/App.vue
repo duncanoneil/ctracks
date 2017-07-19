6f97f3cb1e7a53d9cb57a61b7ab2c6dd6f18bb80
Kieran <kieranwild@gmail.com>
2017-07-19 10:46:04 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app light>
+   <v-navigation-drawer v-model="sideNav">
+     <v-list>
+       <v-list-tile>
+         <v-list-tile-action>
+           <v-icon>supervisor_account</v-icon>
+         </v-list-tile-action>
+         <v-list-tile-content>About</v-list-tile-content>
+       </v-list-tile>
+     </v-list>
+    </v-navigation-drawer>
+    <v-toolbar>
+      <v-toolbar-side-icon
+        @click.native.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>Community Tracks Inverclyde</v-toolbar-title>
+      <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn flat>
+            <v-icon left>supervisor_account</v-icon>
+            About
+          </v-btn>
+          <v-btn flat>Contact</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
+
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+          sideNav: false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
