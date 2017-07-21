4c576e35d85ff11d6bf087aa8818764205bd53d6
Kieran <kieranwild@gmail.com>
2017-07-21 13:03:10 +0100

    image carousel

--- src/components/Front.vue
+++ src/components/Front.vue
@@ -1,15 +1,39 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-sm-right">
+        <v-btn large dark class="accent">News</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-sm-left">
+        <v-btn large dark class="accent">Events</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="(item,i) in items"
+            :src="item.imageUrl"
+            :key="item.id">
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   export default {
-    name: 'hello',
     data () {
       return {
-        msg: 'FRONT'
+        items: [
+          {
+            imageUrl:'https://upload.wikimedia.org/wikipedia/commons/c/c6/A_mountain_bike_styled_e-bike%2C_Cyclotricity_Stealth.jpg', id: 'dsfasfadsfa', title: "new electric bikes"
+          },
+          {
+            imageUrl:'https://upload.wikimedia.org/wikipedia/commons/e/e0/Groningen.Bikes.jpg', id: 'dsfasfadsfa', title: "new bike storage in Greenock"
+          }
+        ]
       }
     }
   }
