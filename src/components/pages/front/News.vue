8270308d817d3b65b5ef1f42dde65a0e46272445
Kieran <kieranwild@gmail.com>
2017-08-09 16:09:19 +0100

    changed card layout

--- src/components/pages/front/News.vue
+++ src/components/pages/front/News.vue
@@ -12,15 +12,14 @@
               <v-card-media
                 :src="card.src"
                 height="200px"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline white--text" v-text="card.title"></span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card-media>
+              >          </v-card-media>
+                <v-card-title primary-title>
+                  <div>
+                    <h3 class="headline mb-0"> <span class="headline black--text" v-text="card.title"></span></h3>
+                    <span v-text="card.body"></span>
+                  </div>
+                </v-card-title>
+
             </v-card>
           </v-flex>
         </v-layout>
@@ -33,9 +32,10 @@
   export default {
     data: () => ({
       cards: [
-        { title: 'Pre-fab homes', src: 'https://s3.eu-west-2.amazonaws.com/comtracks/bike.jpg', flex: 4 },
-        { title: 'Favorite road trips', src: 'https://s3.eu-west-2.amazonaws.com/comtracks/storage.jpg', flex: 4 }
+        { title: 'Pre-fab homes', src: 'https://s3.eu-west-2.amazonaws.com/comtracks/bike.jpg', flex: 4, body:"Blah Blah Blah Blah" },
+        { title: 'Favorite road trips', src: 'https://s3.eu-west-2.amazonaws.com/comtracks/storage.jpg', flex: 4, body:"Blah Blah Blah Blah" }
         ]
     })
   }
 </script>
+
