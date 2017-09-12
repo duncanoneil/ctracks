b3e408ccd9f2d96c1cd7ac4b98d81a328f15797f
Kieran <kieranwild@gmail.com>
2017-09-12 10:15:28 +0100

    bikes page updated

--- src/components/pages/Bikes.vue
+++ src/components/pages/Bikes.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="hello">
     <h3 class="headline black--text ">Bike Workshop</h3>
+      <v-layout row wrap>
+        <v-flex xs12>
+    
+                
+                <div class="flex-grid-halves">
+  <div class="col">
+          <p class="subheading black--text ">Donate your old bikes</p>
+<p>Do you have old bike you can donate to our project? Bike that are donated will be restored by volunteers and given to local schools for children who don’t have bikes themselves.
+<p><a href="/contact">Please get in touch if you can donate a bike</a></p>
 
+          <p class="subheading black--text ">Bike Maintenance Workshop</p>
+          <p>We have bikes maintenance workshops coming soon.</p>
+
+  </div>
+  <div class="col hidden-sm-and-down"><img src="https://s3.eu-west-2.amazonaws.com/comtracks/photos/bikes.jpg" width="400px">.</div>
+</div>
+        </v-flex>
+      </v-layout>
+    
   </div>
 </template>
 
@@ -18,21 +36,28 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
+.flex-grid {
+  display: flex;
+}
+.flex-grid .col {
+  flex: 1;
+}
+.flex-grid-halves {
+  display: flex;
+  justify-content: space-between;
+}
+.flex-grid-halves .col {
+  width: 50%;
+}
 
-  ul {
-    list-style-type: none;
-    padding: 0;
+@media (max-width: 400px) {
+  .flex-grid,
+  .flex-grid-halves {
+    display: block;
+    .col {
+      width: 100%;
+      margin: 0 0 10px 0;
+    }
   }
-
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-
-  a {
-    color: #42b983;
-  }
+}
 </style>
